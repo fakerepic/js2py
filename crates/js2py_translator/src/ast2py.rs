39904f9c4d428e96c0c149771e86fc4d8230139d
tzh <3136374549@qq.com>
2024-11-21 18:50:23 +0800

    supplement translator's code for insertion-sort.js and calculator.js

--- crates/js2py_translator/src/ast2py.rs
+++ crates/js2py_translator/src/ast2py.rs
@@ -57,12 +57,50 @@ impl Ast2Py {
 
     fn translate_statement(&self, statement: &Statement) -> String {
         match statement {
+            Statement::BlockStatement(b) => self.translate_block_statement(b),
+            Statement::IfStatement(i) => self.translate_if_statement(i),
             Statement::EmptyStatement(_) => "".into(),
             Statement::FunctionDeclaration(f) => self.translate_function(f),
             Statement::ReturnStatement(r) => self.translate_return_statement(r),
             Statement::VariableDeclarationStatement(v) => self.translate_variable_declaration(v),
+            Statement::WhileStatement(w) => self.translate_while_statement(w),
+            Statement::ExpressionStatement(e) => self.translate_expression_statement(e),
+            Statement::ContinueStatement(_) => String::from("continue"),
+            Statement::BreakStatement(_) => String::from("break"),
             _ => unimplemented!("unsupported statement {:?}", self.source_of(statement)),
         }
+    }
+
+    fn translate_if_statement(&self, if_statement: &IfStatement) -> String {
+        let test = self.translate_expression(&if_statement.test);
+        let consequent = self.translate_statement(&if_statement.consequent);
+        let alternate = if let Some(alt) = &if_statement.alternate {
+            format!("\nelse:\n{}", make_indent(&self.translate_statement(alt), self.indent))
+        } else {
+            String::new()
+        };
+        format!("if {}:\n{}{}", test, consequent, alternate)
+    }
+
+    fn translate_expression_statement(&self, expression_statement: &ExpressionStatement) -> String {
+        self.translate_expression(&expression_statement.expression)
+    }
+
+    fn translate_block_statement(&self, block_stmt: &BlockStatement) -> String {
+        block_stmt.body
+            .iter()
+            .map(|stmt| self.translate_statement(stmt))
+            .map(|code| make_indent(&code, self.indent))
+            .collect::<Vec<_>>()
+            .join("\n")
+            .trim_end()  // 去掉最后一个多余的换行符
+            .to_string()
+    }
+
+    fn translate_while_statement(&self, while_stmt: &WhileStatement) -> String {
+        let test = self.translate_expression(&while_stmt.test);
+        let body = self.translate_statement(&while_stmt.body);
+        format!("while {}:\n{}", test, body)
     }
 
     fn translate_variable_declaration(&self, var: &VariableDeclaration) -> String {
@@ -89,6 +127,7 @@ impl Ast2Py {
         let body = if let Some(body) = &function.body {
             body.statements
                 .iter()
+                .filter(|stmt| !matches!(stmt, Statement::EmptyStatement(_)))
                 .map(|stmt| self.translate_statement(stmt))
                 .map(|code| make_indent(&code, self.indent))
                 .collect::<Vec<_>>()
@@ -110,25 +149,136 @@ impl Ast2Py {
 
     fn translate_expression(&self, expr: &Expression) -> String {
         match expr {
+            Expression::BooleanLiteral(b) => (if b.value { "True" } else { "False" }).to_string(),
             Expression::NumericLiteral(num) => num.value.to_string(),
+            Expression::StringLiteral(s) => s.value.to_string(),
             Expression::Identifier(id) => id.name.to_string(),
-            Expression::BinaryExpression(bin_expr) => {
-                let lhs = self.translate_expression(&bin_expr.left);
-                let rhs = self.translate_expression(&bin_expr.right);
-                let op = self.translate_binary_operator(bin_expr.operator);
-                format!("{} {} {}", lhs, op, rhs)
-            }
+            Expression::BinaryExpression(bin_expr) => self.translate_binary_expression(bin_expr),
+            Expression::StaticMemberExpression(mem_expr) => self.translate_static_member_expression(mem_expr),
+            Expression::ComputedMemberExpression(mem_expr) => self.translate_computed_member_expression(mem_expr),
+            Expression::ArrayExpression(arr_expr) => self.translate_array_expression(arr_expr),
+            Expression::AssignmentExpression(assign_expr) => self.translate_assignment_expression(assign_expr),
+            Expression::ObjectExpression(obj_expr) => self.translate_object_expression(obj_expr),
+            Expression::CallExpression(call_expr) => self.translate_call_expression(call_expr),
+            Expression::LogicalExpression(logic_expr) => self.translate_logical_expression(logic_expr),
+            Expression::NullLiteral(_) => String::from("None"),
             _ => unimplemented!("unsupported expression {:?}", self.source_of(expr)),
         }
     }
 
-    fn translate_binary_operator(&self, operator: BinaryOperator) -> &'static str {
-        match operator {
-            BinaryOperator::Addition => "+",
-            BinaryOperator::Subtraction => "-",
-            BinaryOperator::Multiplication => "*",
-            BinaryOperator::Division => "/",
-            _ => unimplemented!("unsupported binary operator {:?}", operator),
+    fn translate_computed_member_expression(&self, mem_expr: &ComputedMemberExpression) -> String {
+        let object = self.translate_expression(&mem_expr.object);
+        let expression = self.translate_expression(&mem_expr.expression);
+        format!("{}[{}]", object, expression)
+    }
+
+    fn translate_logical_expression(&self, logic_expr: &LogicalExpression) -> String {
+        let left = self.translate_expression(&logic_expr.left);
+        let right = self.translate_expression(&logic_expr.right);
+        let operator = match logic_expr.operator {
+            LogicalOperator::Or => "or",
+            LogicalOperator::And => "and",
+            _ => unimplemented!("unsupported logical operator {:?}", self.source_of(logic_expr)), // Python 没有 ?? 操作符
+        };
+        format!("{} {} {}", left, operator, right)
+    }
+
+    fn translate_call_expression(&self, call_expr: &CallExpression) -> String {
+        let callee = self.translate_expression(&call_expr.callee);
+    
+        let arguments = call_expr
+            .arguments
+            .iter()
+            .map(|arg| self.translate_expression(arg))
+            .collect::<Vec<_>>()
+            .join(", ");
+
+        // 特殊判断：如果 callee 是 StaticMemberExpression，且 property 为 push
+        if let Expression::StaticMemberExpression(mem_expr) = &call_expr.callee {
+            if mem_expr.property.name == "log" {
+                return format!("print({})", arguments);
+            }
+            if mem_expr.property.name == "push" {
+                let object = self.translate_expression(&mem_expr.object);
+                return format!("{}.append({})", object, arguments);
+            }
         }
+
+        if let Expression::Identifier(id) = &call_expr.callee {
+            if id.name == "parseFloat" {
+                return format!("float({})", arguments);
+            }
+        }
+
+        format!("{}({})", callee, arguments)
+    }
+
+    fn translate_object_expression(&self, obj_expr: &ObjectExpression) -> String {
+        let properties = obj_expr
+            .properties
+            .iter()
+            .map(|prop| {
+                let var_name = match &prop.key {
+                    PropertyKey::IdentifierName(id) => format!("\"{}\"", id.name),
+                    PropertyKey::StringLiteral(s) => format!("{}", s.value),
+                    PropertyKey::NumericLiteral(n) => n.value.to_string(),
+                };
+                let key = var_name;
+                let value = self.translate_expression(&prop.value);
+                format!("{}: {}", key, value)
+            })
+            .collect::<Vec<_>>()
+            .join(", ");
+        format!("{{{}}}", properties)
+    }
+
+    fn translate_array_expression(&self, arr_expr: &ArrayExpression) -> String {
+        let elements = arr_expr
+            .elements
+            .iter()
+            .map(|element| match element {
+                ArrayExpressionElement::Elision(_) => "None".to_string(),
+                ArrayExpressionElement::Expression(expr) => self.translate_expression(expr),
+            })
+            .collect::<Vec<_>>()
+            .join(", ");
+        format!("[{}]", elements)
+    }
+
+    fn translate_static_member_expression(&self, mem_expr: &StaticMemberExpression) -> String {
+        let object = self.translate_expression(&mem_expr.object);
+        let property = mem_expr.property.name.to_string();
+        if property == "length" {
+            return format!("len({})", object);
+        }
+        format!("{}.{}", object, property)
+    }
+
+    fn translate_binary_expression(&self, bin_expr: &BinaryExpression) -> String {
+        let lhs = self.translate_expression(&bin_expr.left);
+        let rhs = self.translate_expression(&bin_expr.right);
+        let op = self.translate_binary_operator(bin_expr.operator);
+        format!("{} {} {}", lhs, op, rhs)
+    }
+
+    fn translate_assignment_expression(&self, assign_expr: &AssignmentExpression) -> String {
+        let left = match &assign_expr.left {
+            AssignmentTarget::Identifier(id) => id.name.to_string(),
+            AssignmentTarget::StaticMemberExpression(mem_expr) => self.translate_static_member_expression(mem_expr),
+            AssignmentTarget::ComputedMemberExpression(mem_expr) => self.translate_computed_member_expression(mem_expr),
+        };
+        let operator = serde_json::to_string(&assign_expr.operator)
+            .unwrap_or_else(|_| unimplemented!("unsupported assignment operator {:?}", assign_expr.operator))
+            .trim_matches('"')
+            .to_string();
+        let right = self.translate_expression(&assign_expr.right);
+        format!("{} {} {}", left, operator, right)
+    }
+
+    fn translate_binary_operator(&self, operator: BinaryOperator) -> String {
+        serde_json::to_string(&operator)
+            .unwrap_or_else(|_| unimplemented!("unsupported binary operator {:?}", operator))
+            .trim_matches('"')
+            .to_string()
     }
 }
--- crates/js2py_translator/src/main.rs
+++ crates/js2py_translator/src/main.rs
@@ -8,37 +8,235 @@ fn main() {
         std::process::exit(1);
     }
 
-    let source_code = std::fs::read_to_string(&args[1]).unwrap();
+    let filename = &args[1];
+
+    let source_code = std::fs::read_to_string(filename).unwrap();
 
     let mut parser = Parser::new(&source_code);
     let ast = parser.parse().unwrap();
 
     let python_code = Ast2Py::default().build(&ast).code;
 
-    println!("{}", python_code);
+    let output_path = format!("{}.py", filename);
+    std::fs::write(output_path, python_code).unwrap();
+
+    println!("wrote python code into {}.py", filename)
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
     #[test]
-    fn t1() {
+    fn test_insertion_sort() {
         let source = r#"
-    function add(a, b) {
-    function y() {
-        return a + b;
+function insertionSort(arr) {
+    let i = 1;
+    while (i < arr.length) {
+        const key = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+        i = i + 1;
     }
-    return y;
-    }
-    function multi(a, b) {
-    return a * b;
-    }
-    let f = 1 + 2;
+    return arr;
+}
+let array = [5.3, 2.1, 8.7, 1.9, 3.4];
+console.log(insertionSort(array));
             "#;
         let mut parser = Parser::new(source);
         let ast = parser.parse().unwrap();
 
-        let python_code = Ast2Py::new().with_indent(2).build(&ast).code;
+        let test_name = "test_insertion_sort";
+        let path = format!("{}.ast.json", test_name);
+        let writer = std::fs::File::create(path).unwrap();
+        serde_json::to_writer_pretty(writer, &ast).unwrap();
+
+        let python_code = Ast2Py::new().with_indent(4).build(&ast).code;
         println!("{}", python_code);
+        let output_path = format!("{}.py", test_name);
+        std::fs::write(output_path, python_code).unwrap();
+    }
+
+    #[test]
+    fn test_calculator() {
+        let source = r#"
+const digitals = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+function isDigital(c) {
+    var i = 0;
+    var j = 1;
+    while (i < digitals.length) {
+        if (digitals[i] == c) {
+            return true;
+        }
+        i += 1;
+    }
+    var k = 3;
+    return false;
+}
+
+const operators = ['+', '-', '*', '/'];
+
+function isOperator(c) {
+    var i = 0;
+    while (i < operators.length) {
+        if (operators[i] == c) {
+            return true;
+        }
+        i += 1;
+    }
+    return false;
+}
+
+function calculateExpression(s) {
+    // 定义运算符的优先级
+    const precedence = {
+        '+': 10,
+        '-': 10,
+        '*': 20,
+        '/': 20
+    };
+
+    // 将中缀表达式转换为后缀表达式（逆波兰表达式）
+    function infixToPostfix(expression) {
+        const output = [];
+        const operatorStack = [];
+        var i = 0;
+        while (i < expression.length) {
+            const c = expression[i];
+
+            if (isDigital(c)) {
+                // 处理多位数
+                var num = '';
+                while (i < expression.length && isDigital(expression[i])) {
+                    num = num + expression[i];
+                    i += 1;
+                }
+                output.push(num);
+                continue; // 已经移动了 i
+            } else if (c == '(') {
+                operatorStack.push(c);
+            } else if (c == ')') {
+                while (operatorStack.length > 0 && operatorStack[operatorStack.length - 1] != '(') {
+                    var top = operatorStack.pop();
+                    output.push(top);
+                }
+                if (operatorStack.length > 0 && operatorStack[operatorStack.length - 1] == '(') {
+                    operatorStack.pop(); // 弹出 '('
+                } else {
+                    return null;
+                }
+            } else if (isOperator(c)) {
+                while (
+                    operatorStack.length > 0 &&
+                    isOperator(operatorStack[operatorStack.length - 1]) &&
+                    precedence[operatorStack[operatorStack.length - 1]] >= precedence[c]
+                ) {
+                    var top = operatorStack.pop();
+                    output.push(top);
+                }
+                operatorStack.push(c);
+            } else {
+                return null
+            }
+            i += 1;
+        }
+
+        // 弹出剩余的运算符
+        while (operatorStack.length > 0) {
+            const op = operatorStack.pop();
+            if (op == '(' || op == ')') {
+                return null
+            }
+            output.push(op);
+        }
+
+        return output;
+    }
+
+    // 计算后缀表达式的值
+    function evaluatePostfix(postfix) {
+        const stack = [];
+
+        var i = 0;
+        while (i < postfix.length) {
+            const token = postfix[i];
+            if (isOperator(token)) {
+                if (stack.length < 2) {
+                    return null
+                }
+                var top = stack.pop();
+                const b = parseFloat(top);
+                top = stack.pop();
+                const a = parseFloat(top);
+                var result = 0;
+                if (token == '+') {
+                    result = a + b;
+                } else if (token == '-') {
+                    result = a - b;
+                } else if (token == '*') {
+                    result = a * b;
+                } else if (token == '/') {
+                    if (b == 0) {
+                        return null
+                    }
+                    result = a / b;
+                } else {
+                    return null
+                }
+                stack.push(result);
+            } else {
+                stack.push(token);
+            }
+            i += 1;
+        }
+
+        if (stack.length != 1) {
+            return null
+        }
+
+        return stack.pop();
+    }
+
+    const postfix = infixToPostfix(s);
+    return evaluatePostfix(postfix);
+}
+
+const expressions = [
+    "3+4*2/(1-5)", // 1
+    "(2+3)*4", // 20
+    "10+2*6", // 22
+    "100*(2+12)/14" // 100
+];
+
+var i = 0;
+while (i < expressions.length) {
+    const expr = expressions[i];
+    const result = calculateExpression(expr);
+    if (result == null) {
+        console.log('failed to calculate the expression');
+        i += 1;
+        continue;
+    }
+    console.log('result: ');
+    console.log(result);
+    i += 1;
+}
+            "#;
+        let mut parser = Parser::new(source);
+        let ast = parser.parse().unwrap();
+
+        let test_name = "test_calculator";
+        let path = format!("{}.ast.json", test_name);
+        let writer = std::fs::File::create(path).unwrap();
+        serde_json::to_writer_pretty(writer, &ast).unwrap();
+
+        let python_code = Ast2Py::new().with_indent(4).build(&ast).code;
+        println!("{}", python_code);
+        let output_path = format!("{}.py", test_name);
+        std::fs::write(output_path, python_code).unwrap();
     }
 }
